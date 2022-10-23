775efe3bdd8dd7c9340e81e482c64c942d87f858
guilherme.dornelles <guilherme.dornelles@nubo.ai>
2022-10-23 19:20:24 -0300

    Removing comments

--- src/Server.java
+++ src/Server.java
@@ -5,15 +5,11 @@ import java.rmi.registry.LocateRegistry;
 public class Server {
     public Server() {
         try {
-            // Definicao do ip onde o servico ira funcionar
             System.setProperty("java.rmi.server.hostname", "localhost");
 
-            // Registro do servico em uma porta
             LocateRegistry.createRegistry(1099);
-            // Cria o objeto que implementa os metodos que serao servidos
             IBank bank = new Bank();
             mockData(bank);
-            // Coloca na porta registrada o servico da calculadora
             Naming.bind("BankService", (Remote) bank);
             System.out.println("Conexão estabelecida!");
 
